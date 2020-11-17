1520b5ea934a835ecea3374aab6fb6b24cd23559
Piotr Kowańdy <phoennix@Phoennix-Mac.local>
2020-11-17 19:10:59 +0100

    build folder removed

--- reverse_string/build/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/phoennix/Desktop/cmake_projects/reverse_string/lib/googletest/googlemock
-# Build directory: /Users/phoennix/Desktop/cmake_projects/reverse_string/build/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- reverse_string/build/src/CMakeFiles/reverse_string_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libreverse_string_lib.a"
-)
